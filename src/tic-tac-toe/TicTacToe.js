ed881dff65cd02f1e7b9d68ee45163dce6a1f3ce
elenaBushueva <helenabushueva@gmail.com>
2021-12-28 17:25:26 -0800

    Tic-tac-toe

--- src/tic-tac-toe/TicTacToe.js
+++ src/tic-tac-toe/TicTacToe.js
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
 import {nanoid} from "nanoid";
-import Board from "../inputs/Board";
+import Board from "./Board";
 
-//creating an array with 9 objects; [{}, {}, {}, {}, {}, {}, {}, {}, {id: , value: null}]
 const initialBoard = Array(9).fill({})
     .map(el => ({id: nanoid(), value: null}));
+
 
 const TicTacToe = () => {
 
     const [board, setBoard] = useState(initialBoard);           // to change value of a square
     const [countMoves, setCountMoves] = useState(1);   //  odd or even to choose "X" or "O"
+    const xOrO = countMoves % 2 ? 'X' : 'O';                   // odd or even === "X" or "O"
 
-    //
     const handlerMove = (id) => {
-        const xOrO = countMoves % 2 ? 'X' : 'O';                // odd or even === "X" or "O"
         const newBoard = board.map(el => el.id === id ?         // id of a square change value on "X" or "O"
             {...el, value: xOrO} : el);
         setBoard(newBoard);
@@ -22,18 +21,12 @@ const TicTacToe = () => {
 
     const checkWinner = () => {
         const arr = [                                          // array of indexes of winning combinations
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
         ];
         for (let i = 0; i < arr.length; i++) {
             const [a, b, c] = arr[i];                         // call every winning combination of indexes as [a, b, c]
-
             if (board[a].value === board[b].value && board[b].value === board[c].value)
                 return board[a].value;                        // if indexes are equal -- func returns value of equals
         }
@@ -41,8 +34,6 @@ const TicTacToe = () => {
     };
 
     const winner = checkWinner();
-
-    console.log(winner);
 
     const startAgain = () => {
         setBoard(initialBoard);
@@ -52,7 +43,7 @@ const TicTacToe = () => {
     return (
         <div>
             <h2>Tic Tac Toe</h2>
-            {winner === null ? <p>Next is: {countMoves % 2 ? 'X' : 'O'}</p> : <p>{winner} has won!</p>}
+            {winner === null ? <p>Next is: {xOrO}</p> : <p>{winner} has won!</p>}
             {winner !== null && <button onClick={startAgain}> Start a new game </button>}
             <Board board={board} handlerMove={handlerMove}/>
         </div>
